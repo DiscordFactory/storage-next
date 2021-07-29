27b62c193ca703c62136683fc7f940089a8addb7
Freeze455 <baptiste.parmantier.pro@gmail.com>
2021-07-29 18:19:26 +0200

    :bug: Fix Cyan color

--- src/colors/Cyan.ts
+++ src/colors/Cyan.ts
@@ -1,13 +1,13 @@
 enum Cyan {
-  CYAN_100 = '#ECFCCB',
-  CYAN_200 = '#D9F99D',
-  CYAN_300 = '#BEF264',
-  CYAN_400 = '#A3E635',
-  CYAN_500 = '#84CC16',
-  CYAN_600 = '#65A30D',
-  CYAN_700 = '#4D7C0F',
-  CYAN_800 = '#3F6212',
-  CYAN_900 = '#365314'
+  CYAN_100 = '#CFFAFE',
+  CYAN_200 = '#A5F3FC',
+  CYAN_300 = '#67E8F9',
+  CYAN_400 = '#22D3EE',
+  CYAN_500 = '#06B6D4',
+  CYAN_600 = '#0891B2',
+  CYAN_700 = '#0E7490',
+  CYAN_800 = '#155E75',
+  CYAN_900 = '#164E63'
 }
 
 export default Cyan
