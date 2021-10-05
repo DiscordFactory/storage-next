7e75c6701f9b51a0830889e6f99c6c1601522f6a
leadcodedev <baptiste.parmantier.pro@gmail.com>
2021-10-05 21:39:10 +0200

    :bug: Fix bug in production

--- src/managers/ModelManager.ts
+++ src/managers/ModelManager.ts
@@ -4,13 +4,16 @@ import { fetch } from 'fs-recursive'
 import Storage from '../Storage'
 
 export default class ModelManager {
-  public migrations: MigrationEntity[] = []
-
   constructor (public storage: Storage) {
   }
 
   public async initialize () {
-    const baseDir = path.join(process.cwd(), 'src')
+    const baseDir = path.join(
+      process.cwd(),
+      process.env.NODE_ENV === 'production'
+        ? path.join('build', 'src')
+        : 'src'
+    )
 
     const fetchedFiles = await fetch(
       baseDir,
