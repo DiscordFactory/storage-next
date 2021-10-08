e0941bb24db86d33948aad6fe17aea85a4e0245c
Freeze455 <baptiste.parmantier.pro@gmail.com>
2021-10-08 08:32:31 +0200

    :construction: Work in progress

--- src/queries/Related.ts
+++ src/queries/Related.ts
@@ -1,22 +1,41 @@
 import { QueryBuilder } from '../QueryBuilder'
-import { Knex } from 'knex'
 import Uuid from '../Uuid'
 
 export default class Related {
   constructor (private relationModelName: string, private queryBuilder: QueryBuilder<unknown>) {
   }
 
-  public async attach (...ids: string[] | number[]) {
-    const pivotTableName = [this.queryBuilder.model.tableName, this.relationModelName.slice(0, this.relationModelName.length - 1)]
-      .sort()
-      .join('_')
+  private serializePivotTableName () {
+    const modelName = this.queryBuilder.model.tableName
+    const relationModelName = this.relationModelName.slice(0, this.relationModelName.length - 1)
+    return {
+      modelName,
+      relationModelName,
+      pivotTableName: [modelName, relationModelName].sort().join('_')
+    }
+  }
+
+  public async attach (ids: string[] | number[]): Promise<void> {
+    const { relationModelName, pivotTableName } = this.serializePivotTableName()
 
     await this.queryBuilder.getQuery()
       .from(pivotTableName)
       .insert(ids.map((id: string | number) => ({
         id: Uuid.generateV4(),
         [`${this.queryBuilder.model.tableName}Id`]: '240561194958716928',
-        [`${this.relationModelName.slice(0, this.relationModelName.length - 1)}Id`]: id
+        [`${relationModelName}Id`]: id
       })))
+  }
+
+  public async detach (ids: string[] | number[]): Promise<void> {
+    const { relationModelName, pivotTableName } = this.serializePivotTableName()
+    await Promise.all(
+      ids.map(async (id: string | number) => {
+        await this.queryBuilder.getQuery()
+          .from(pivotTableName)
+          .where({ [`${relationModelName}Id`]: id })
+          .delete()
+      })
+    )
   }
 }
