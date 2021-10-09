91df26d51a7a2f0094537c43bf91feb7bac7a673
Freeze455 <baptiste.parmantier.pro@gmail.com>
2021-10-09 09:38:00 +0200

    :pencil: Write jsdoc

--- src/entities/Model.ts
+++ src/entities/Model.ts
@@ -121,18 +121,38 @@ export function manyToMany (relation: typeof BaseModel, options?: RelationOption
 }
 
 export abstract class BaseModel {
+  /**
+   * @description Knex query builder entry point
+   * @see QueryInterface
+   * @return Knex.QueryBuilder<ObjectResolvable , any>
+   */
   public static query (): Knex.QueryBuilder<ObjectResolvable , any> {
     return BaseModel.query()
   }
 
+  /**
+   * @description Retrieves the entire contents of the table that the model represents
+   * @return Promise
+   */
   public static findAll (): Promise<BaseModel[]> {
     return BaseModel.findAll()
   }
 
+  /**
+   * @description Retrieves one resource of the table that the model represents
+   * @param {TypeResolvable} value
+   * @see TypeResolvable
+   */
   public static async find (value: TypeResolvable): Promise<BaseModel> {
     return BaseModel.find(value)
   }
 
+  /**
+   * @description Retrieves a resource according to a column and a value
+   * @param {ObjectResolvable} data
+   * @param {string} columnName
+   * @param {string|boolean|number} value
+   */
   public static findBy (data: ObjectResolvable): Promise<BaseModel>
   public static findBy (columnName: string, value: TypeResolvable): Promise<BaseModel>
   public static findBy (data: ObjectResolvable | string, value?: TypeResolvable): Promise<BaseModel> {
@@ -143,27 +163,63 @@ export abstract class BaseModel {
     }
   }
 
+  /**
+   * @description Create one resource from passed data
+   * @param {ObjectResolvable} data
+   * @return Promise
+   */
   public static async create (data: ObjectResolvable): Promise<BaseModel> {
     return BaseModel.create(data)
   }
 
+  /**
+   * @description Create many resource from passed data
+   * @param {ObjectResolvable[]} data
+   */
   public static async createMany (data: ObjectResolvable[]): Promise<BaseModel[]> {
     return BaseModel.createMany(data)
   }
 
-  public static beforeInsert (model: BaseModel) {}
-  public static beforeSave (model: BaseModel) {}
+  /**
+   * @description Hook called at each new data insertion in database
+   * @param {BaseModel} model
+   */
+  public static beforeInsert (model: BaseModel): void {}
 
+  /**
+   * @description Hook called at each save data insertion in database
+   * @param {BaseModel} model
+   */
+  public static beforeSave (model: BaseModel): void {}
+
+  /**
+   * @description Updating a resource from the linked model
+   * @param {ObjectResolvable} data
+   * @return Promise
+   */
   public async update (data: ObjectResolvable): Promise<BaseModel> {
     return this
   }
 
+  /**
+   * @description Deleting a resource from the linked model
+   * @return Promise
+   */
   public async delete (): Promise<void> {}
 
+  /**
+   * @description Access point to methods linked to a relationship
+   * @param {string} relationName
+   * @return Promise
+   */
   public related (relationName: string): Related<BaseModel> {
     return '' as any
   }
 
+  /**
+   * @description Serialize the relationship passed in parameter
+   * @param alias
+   */
   public async preload (alias: string): Promise<BaseModel> {
     return this
   }
